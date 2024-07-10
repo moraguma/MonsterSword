2554a458cb4a6761824294ccdf46d1f6f2feb95c
Moraguma <g170603@dac.unicamp.br>
2024-07-10 13:35:22 -0300

    Implements menu sounds

--- addons/jam_essentials/scenes/scripts/CameraFocusButton.gd
+++ addons/jam_essentials/scenes/scripts/CameraFocusButton.gd
@@ -4,5 +4,11 @@ extends Button
 @export var aim_pos: Vector2
 
 
+func _ready():
+	mouse_entered.connect(SoundController.play_sfx.bind("UIHover"))
+	pressed.connect(SoundController.play_sfx.bind("UIPlay"))
+
+
 func _pressed():
 	GlobalCamera.follow_pos(aim_pos)
+
--- addons/jam_essentials/scenes/scripts/TransitionButton.gd
+++ addons/jam_essentials/scenes/scripts/TransitionButton.gd
@@ -4,5 +4,10 @@ extends Button
 @export var transition_path: String
 
 
+func _ready():
+	mouse_entered.connect(SoundController.play_sfx.bind("UIHover"))
+	pressed.connect(SoundController.play_sfx.bind("UIPlay"))
+
+
 func _pressed():
 	SceneManager.goto_scene(transition_path)
